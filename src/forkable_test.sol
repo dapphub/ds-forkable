3fd1ba8d8075b86f03e9248c94a876e48a6c5675
Nikolai Mushegian <nikolai@nexusdev.us>
2016-09-09 09:10:44 -0400

    clarify test

--- src/forkable_test.sol
+++ src/forkable_test.sol
@@ -3,22 +3,27 @@ import 'forkable.sol';
 
 contract ForkableDatastoreTest is Test {
     ForkableDatastoreService data;
+    uint32 branch1;
+    uint32 branch2;
     function setUp() {
         data = new ForkableDatastoreService();
-
+        branch1 = data.fork(0);
+        data.set(branch1, "A", "10");
+        data.set(branch1, "B", "20");
+        data.set(branch1, "C", "30");
     }
     function testBasics() {
         assertEq32(0, data.get(0, "unused_key"));
-        uint32 branch1 = data.fork(0);
         assertEq(uint(branch1), 1);
-        data.set(branch1, "A", "10");
-        data.set(branch1, "B", "20");
         assertEq32(data.get(branch1, "A"), "10");
-        var branch2 = data.fork(1);
+    }
+    function testForkThenSet() {
+        branch2 = data.fork(1);
         assertEq(uint(branch2), 2);
         data.set(branch1, "A", "11");
         assertEq32(data.get(branch1, "A"), "11");
         assertEq32(data.get(branch2, "A"), "10");
-
+        assertEq32(data.get(branch1, "B"), "20");
+        assertEq32(data.get(branch2, "B"), "20");
     }
 }
